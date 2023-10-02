67b4ac320bc12863903abb0c3f429610e833492e
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-10-02 19:10:53 -0300

    feature: Estrutura visual da home p1

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,19 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './style'
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre
+        </Text>
+      </Hero>
+
+      <Preview></Preview>
+    </Container>
+  )
+}
