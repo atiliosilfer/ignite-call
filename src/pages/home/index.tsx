7a688761874c9b8670cb273c2ccc9c3e70d162ab
atiliosilfer <atilio.silvaferreira@gmail.com>
2023-10-24 19:46:22 -0300

    Estrutura visual da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,30 @@
 import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './style'
+import Image from 'next/image'
 import previewImage from '../../assets/app-preview.png'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="lg">
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre
         </Text>
       </Hero>
 
-      <Preview></Preview>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando aplicação em funcionamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
     </Container>
   )
 }
